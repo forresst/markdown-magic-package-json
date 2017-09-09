0c94ffb4fe4a3b18565a24baf741d90b59d50279
forresst <forresst17@gmail.com>
2017-09-09 08:51:46 +0200

    refactor: improve the code and add docs into the code

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ const fs = require('fs');
 const path = require('path');
 const findup = require('findup');
 
-const findPkg = dir => {
+/**
+ * Return the package.json's path or undefined if not found
+ *
+ * Note: The search is always done in the ancestors of the file tree
+ *
+ * @param {string} [dir=process.cwd()] Path where the file search starts
+ * @returns {string} package.json's path or undefined if not found
+ */
+const findPathPkg = (dir = process.cwd()) => {
 	try {
-		return path.join(findup.sync(dir, 'package.json'), 'package.json');
+		return path.join(findup.sync(path.resolve(process.cwd(), dir), 'package.json'), 'package.json');
 	} catch (err) {
 		return undefined;
 	}
 };
 
-const getPackage = options => {
-	let pkgPath;
-	if (options.pkg) {
-		pkgPath = findPkg(path.resolve(process.cwd(), options.pkg));
-	} else {
-		pkgPath = findPkg(process.cwd());
-	}
+/**
+ * Return the JSON content of package.json or empty JSON if not found the file package.json
+ *
+ * @param {string} [pathSearchStart] Path where the file search starts
+ * @returns {Object} JSON content of package.json
+ */
+const getJsonPkg = pathSearchStart => {
+	const pkgPath = findPathPkg(pathSearchStart);
 
 	let contentJson = {};
 	if (pkgPath) {
@@ -27,7 +36,25 @@ const getPackage = options => {
 	return contentJson;
 };
 
-const deepAccess = (obj, key) => {
+/**
+ * Return the property from object via a string key
+ *
+ * For example:
+ *
+ * const obj = {
+ * 	"a": "Value a",
+ *  "child": {
+ * 		"b": "Value b"
+ * 	}
+ * }
+ *
+ * getDeepAccessProp(obj, 'child.b'); // -> 'Value b'
+ *
+ * @param {Object} obj
+ * @param {string} key
+ * @returns {Object} property
+ */
+const getDeepAccessProp = (obj, key) => {
 	return key.split('.').reduce((nestedObject, key) => {
 		if (nestedObject && key in nestedObject) {
 			return nestedObject[key];
@@ -36,6 +63,19 @@ const deepAccess = (obj, key) => {
 	}, obj);
 };
 
+/**
+ * Plugin markdown-magic-package-json
+ *
+ * @param {string} content content before transformation
+ * @param {Object} options options passed to the transform
+ * @param {string} [options.prop] any property in package.json (like `name`, `version`, `scripts.test`, ...)
+ * @param {string} [options.before] string to add before the property
+ * @param {string} [options.after] string to add after the property
+ * @param {string} [options.unknownTxt] string to add if the property is unknown
+ * @param {string} [options.pkg] `package.json` path. If the path is incorrect, the plugin find `package.json` in ancestor's dir
+ * @param {Object} [config] markdown-magic configuration object
+ * @returns {string} content modified
+ */
 module.exports = (content, options, config) => {
 	if (!options) {
 		options = {};
@@ -51,8 +91,8 @@ module.exports = (content, options, config) => {
 
 	let prop;
 	if (options.prop) {
-		const result = getPackage(options, config);
-		prop = deepAccess(result, options.prop);
+		const jsonPkg = getJsonPkg(options.pkg, config);
+		prop = getDeepAccessProp(jsonPkg, options.prop);
 	}
 
 	if (prop === undefined && options.unknownTxt) {
